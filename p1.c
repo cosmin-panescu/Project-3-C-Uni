75b8c2a4fc91ce2f73768e263410bcb6a67be135
Panescu Cosmin <1panescu.cosmin@gmail.com>
2023-04-27 10:36:17 +0300

    Problema 2

---
+++ p1.c
@@ -0,0 +1,65 @@
+// Scrieti un program care citeste de la tastatura un sir de caractere si il afiseaza in ordine inversa folosind o stiva.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE 101
+
+typedef struct Stack
+{
+    char data[MAX_SIZE];
+    int top;
+} Stack;
+
+void push(Stack *stack, char c)
+{
+    if (stack->top < MAX_SIZE - 1)
+    {
+        stack->top++;
+        stack->data[stack->top] = c;
+    }
+    else
+    {
+        printf("Nu se poate împinge elementul pe stivă: stiva este plină.\n");
+    }
+}
+
+char pop(Stack *stack)
+{
+    if (stack->top >= 0)
+    {
+        char c = stack->data[stack->top];
+        stack->top--;
+        return c;
+    }
+    else
+    {
+        printf("Nu se poate prelua elementul din stivă: stiva este goală.\n");
+        return '\0';
+    }
+}
+
+int main()
+{
+    Stack stack;
+    stack.top = -1;
+    char input[MAX_SIZE];
+
+    printf("Introduceti sirul de caractere dorit: ");
+    fgets(input, MAX_SIZE, stdin);
+
+    for (int i = 0; i < strlen(input) - 1; i++)
+    {
+        push(&stack, input[i]);
+    }
+
+    printf("Sirul de caractere inversat este: ");
+
+    while (stack.top >= 0)
+    {
+        printf("%c", pop(&stack));
+    }
+
+    return 0;
+}
